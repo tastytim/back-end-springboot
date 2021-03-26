ebde73032dc2680cfa0409beb10f7bbcd71a05a1
tastytim <61064400+tastytim@users.noreply.github.com>
2021-03-26 22:25:31 +0100

    Initial commit

---
+++ src/test/java/javatestweabapp/backendspringboot/BackEndSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package javatestweabapp.backendspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
